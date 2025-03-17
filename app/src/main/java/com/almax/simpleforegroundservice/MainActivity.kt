6a763558321a8e390060f24c2a41438c88c65496
maxi <arora.anubhav93@gmail.com>
2025-03-17 18:23:43 +0530

    Update activity to start and stop the service

--- app/src/main/java/com/almax/simpleforegroundservice/MainActivity.kt
+++ app/src/main/java/com/almax/simpleforegroundservice/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.almax.simpleforegroundservice
 
+import android.Manifest
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.almax.simpleforegroundservice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupUi()
+    }
+
+    private fun setupUi() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS), 200
+            )
+        }
+
+        binding.apply {
+            btnStart.setOnClickListener {
+                startForegroundService(Intent(this@MainActivity, ForegroundService::class.java))
+            }
+            btnStop.setOnClickListener {
+                stopService(Intent(this@MainActivity, ForegroundService::class.java))
+            }
+        }
     }
 }
